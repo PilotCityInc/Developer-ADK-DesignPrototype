36e1eb280217857b688cdbcc2e84f57fb56990d7
Derick Lee <dericklee@pilotcity.com>
2021-04-04 04:49:02 -0700

    1.0.33

--- src/Module/Module.vue
+++ src/Module/Module.vue
@@ -344,7 +344,7 @@ export default defineComponent({
     const moduleName = ref('Make');
     const page = reactive({
       subpages: ['Setup', 'Presets'],
-      currentPage: 'Setup'
+      currentPage: 'Presets'
     });
     if (props.userType === 'organizer') {
       page.currentPage = 'setup';
